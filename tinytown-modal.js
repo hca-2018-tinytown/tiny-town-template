b16adea68b6c486f8bde49566f4f564bf9e56321
Kinman Covey <kinman.covey@evhc.net>
2018-04-13 10:34:16 -0500

    added submission detection, cookie support

---
+++ tinytown-modal.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const modal = document.getElementById('emailModal');
+const closeBtn = document.getElementById('close-btn');
+const noThanks = document.getElementById('no-thanks');
+const header = document.getElementById('header');
+
+header.style = "z-index: 0;";
+
+// Close the modal when you click on the x button
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+  header.style = "";
+}
+
+// Close the modal when you click on 'no thanks'
+noThanks.onclick = function () {
+  modal.style.display = "none";
+  header.style = "";
+}
+
+// Close the modal when you click anywhere outside the modal
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    header.style = "";
+  }
+}
+
+// Upon submission, poll for the success or error alerts
+// and close the modal accordingly
+window.addEventListener("load", function() {
+    document.querySelector("input[name='emma_form_submit']").addEventListener("click", function(e) {
+        if(document.querySelector(".emma-status")) {
+            document.querySelector(".emma-status").remove();
+        }
+
+        var pollInterval = window.setInterval(function() {
+            console.log("> polling");
+            if(document.querySelector(".emma-status.emma-alert")) {
+                console.log("> found error message");
+                window.clearInterval(pollInterval);
+            }
+            else if(document.querySelector(".emma-status")) {
+                console.log("> found success message");
+                window.clearInterval(pollInterval);
+
+                window.setTimeout(function() {
+                    modal.style.display = "none";
+                    header.style = "";
+                }, 2000);
+            }
+        }, 250);
+    });
+});
