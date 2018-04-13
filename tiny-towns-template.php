54ca437a9a0c3491e0ea080ee6e1548baa81a189
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-04-13 10:36:45 -0500

    restyled modal

--- tiny-towns-template.php
+++ tiny-towns-template.php
@@ -54,7 +54,7 @@ get_header();
 					<!-- modal starts here -->
 					<div id="emailModal" class="modal">
 						<div class = "modal-content">
-							<span id="close-btn">&times;</span>
+							<span id="close-btn" class = "close">&times;</span>
 							<h5 class = "modal-text">Take Tiny Town Home With You</h5>
 							<p>Sign up to recieve updates from the discovery center</p>
 							<?php echo do_shortcode('[emma_form]'); ?>
